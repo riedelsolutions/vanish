155fad31348443ac7f444f1e737d13f86d2c0f80
riedelsolutions <38634824+riedelsolutions@users.noreply.github.com>
2018-08-31 00:13:50 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+
+function openDiv(id){
+	if(document.getElementById(id+ "Info").className = "info"){
+	document.getElementById(id + "Info").style.display="block";
+	document.getElementById(id+"Info").className = "displaying";
+	}else{
+		document.getElementById.className = "info";
+	}
+}
