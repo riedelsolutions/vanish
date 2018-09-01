98a00aee7fc39d625e45d3948a7b721593e93dd8
riedelsolutions <38634824+riedelsolutions@users.noreply.github.com>
2018-09-01 03:25:21 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-
 function openDiv(id) {
-    var getI = document.getElementById(id+"Info");
+    getI = document.getElementById(id+"Info");
     if (getI.style.display === "block") {
         getI.style.display = "none";
     } else {
         getI.style.display = "block";
     }
-	}
+}
+
+
+function closePopup(){
+	for(var i = 0; i < document.getElementsByClassName('hidden').length; i++){
+	var getClass =document.getElementsByClassName('hidden')[i];
+	if (getClass.style.display === "none") {
+    } else {
+        getClass.style.display = "none";
+    }
+		}
+}
